d5fda3431778658791472a1cb694e4466ff29881
Eduardo Dicarte <eduardo_dicarte@yahoo.com.br>
2016-05-26 01:06:47 -0300

    Definição da versão

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,10 +1,13 @@
 class dc_firewall::install{
+  
+  $iptablesVersion = '1.4.21-16.el7'
+  
   package {'firewalld':
     ensure => absent
   }
   
   package { 'iptables-services':
-    ensure  => present,
+    ensure  => $iptablesVersion,
     require => Package['firewalld']
   }
   
