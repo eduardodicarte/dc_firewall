c6db2d6063f563317b7908167b71159947543856
Eduardo Dicarte <eduardo_dicarte@yahoo.com.br>
2016-05-25 23:17:41 -0300

    Manipulação das regras do iptables através de uma lista

--- manifests/config/rules/accept.pp
+++ manifests/config/rules/accept.pp
@@ -1,33 +1,32 @@
 class dc_firewall::config::rules::accept {
-  $indexTable = 0
-  $indexPort = 1
-  
-  $configs = split($dc_firewall::configAccept,',')
-  
-  $configs.each | String $config |{
+  $indexLineNumber = 0
+  $indexChain = 1
+  $indexPort = 2
+
+  $configs = split($dc_firewall::configAccept, ',')
+
+  $configs.each |String $config| {
     $configItem = split($config, ":")
-    
-    $table = $configItem[$indexTable]
-    notice($table)
-    
+
+    $lineNumber = $configItem[$indexLineNumber]
+    $chain = $configItem[$indexChain]
     $port = $configItem[$indexPort]
-    notice($port) 
-  }
 
-  $msgPorta = "Liberacao da porta $dc_firewall::configAccept"
-  $msg = "iptables -t filter -p tcp -I INPUT 1 -j ACCEPT -m comment --comment '$msgPorta'"
-  $cmdUnless = "iptables -L | grep 'Liberacao da porta'" 
+    $msgPorta = "Liberacao da porta $port"
+    $msg = "iptables -t filter -p tcp -I $chain $lineNumber --dport $port -j ACCEPT -m comment --comment '$msgPorta'"
+    $cmdUnless = "iptables -L | grep '$msgPorta'"
 
-  exec{"libera_porta":
-    command => $msg,
-    path => ['/usr/sbin/','/usr/bin/'],
-    unless => $cmdUnless
-  }
-  
-  exec{"iptables_save_rules":
-    command => "service iptables save",
-    path =>  ['/usr/sbin/','/usr/bin/'],
-    unless => $cmdUnless,
-    require => Exec["libera_porta"],
+    exec { "libera_porta_$port":
+      command => $msg,
+      path    => ['/usr/sbin/', '/usr/bin/'],
+      unless  => $cmdUnless
+    }
+
+    exec { "iptables_save_rules_port_$port":
+      command => "service iptables save",
+      path    => ['/usr/sbin/', '/usr/bin/'],
+      unless  => $cmdUnless,
+      require => Exec["libera_porta_$port"],
+    }
   }
 }
